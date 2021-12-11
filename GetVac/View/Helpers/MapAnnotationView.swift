2100cb2d7251869b1987adbc7d662fe8e46ff5d2
Arthur Neves <arthurnvsm@gmail.com>
2021-12-11 18:24:43 -0300

    Map annotation implementation and Map minor updates

--- GetVac/View/Helpers/MapAnnotationView.swift
+++ GetVac/View/Helpers/MapAnnotationView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct MapAnnotationView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+  @State private var animation: Double = 0.0
+  
+  var body: some View {
+    ZStack {
+      Circle()
+        .fill(Color.accentColor)
+        .frame(width: 24, height: 24, alignment: .center)
+      
+      Circle()
+        .stroke(Color.accentColor, lineWidth: 2)
+        .frame(width: 22, height: 22, alignment: .center)
+        .scaleEffect(1 + CGFloat(animation))
+        .opacity(1 - animation)
+      
+      Image(systemName: "mappin.and.ellipse")
+        .resizable()
+        .scaledToFit()
+        .frame(width: 15, height: 15, alignment: .center)
+        .clipShape(Circle())
+        .foregroundColor(Color.white)
+    } //: ZStack
+    .onAppear {
+      withAnimation(Animation.easeOut(duration: 1.2).repeatForever(autoreverses: false)) {
+        animation = 1
+      }
     }
+  }
 }
 
 struct MapAnnotationView_Previews: PreviewProvider {
-    static var previews: some View {
-        MapAnnotationView()
-    }
+  static var previews: some View {
+    MapAnnotationView()
+  }
 }
--- GetVac/View/Helpers/MapView.swift
+++ GetVac/View/Helpers/MapView.swift
@@ -11,10 +11,13 @@ import MapKit
 
 struct MapView: View {
   var coordinate: CLLocationCoordinate2D
+  let point = [
+    AnnotatedItem(name: "Location", coordinate: .init(latitude: -15.742946310268854, longitude: -47.891445397031276))
+  ]
   
   @State private var region = MKCoordinateRegion(
-    center: CLLocationCoordinate2D(latitude: -47.891445397031276, longitude: -15.742946310268854),
-    span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+    center: CLLocationCoordinate2D(latitude: -15.742946310268854, longitude: -47.891445397031276),
+    span: MKCoordinateSpan(latitudeDelta: 0.02, longitudeDelta: 0.02)
   )
   
   private func setRegion(_ coordinate: CLLocationCoordinate2D) {
@@ -25,15 +28,25 @@ struct MapView: View {
   }
   
   var body: some View {
-    Map(coordinateRegion: $region)
+    Map(coordinateRegion: $region, annotationItems: point, annotationContent: {item in
+      MapAnnotation(coordinate: coordinate) {
+        MapAnnotationView()
+      }
+    })
       .onAppear {
         setRegion(coordinate)
       }
   }
 }
 
+struct AnnotatedItem: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+}
+
 struct MapView_Previews: PreviewProvider {
   static var previews: some View {
-    MapView(coordinate: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868))
+    MapView(coordinate: CLLocationCoordinate2D(latitude: -15.742946310268854, longitude: -47.891445397031276))
   }
 }
